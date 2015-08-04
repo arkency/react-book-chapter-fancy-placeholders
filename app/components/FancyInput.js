6f7ea24c0b7aaab8d2a53d805c261ef7e24c2604
Fernando <krajka.bartosz@gmail.com>
2015-08-04 15:42:35 +0200

    Preparing environment. The component is located in /components/FancyInput.js

---
+++ app/components/FancyInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FancyInput extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default FancyInput;
--- app/index.js
+++ app/index.js
@@ -1,26 +1,8 @@
-import 'normalize.css'
-import './styles.css'
+import './styles.css';
+import React from 'react';
+import FancyInput from './components/FancyInput';
 
-import debug from 'debug'
-import React from 'react'
-import Hello from './components/Hello'
+const DIV = document.createElement('div');
+document.body.appendChild(DIV);
 
-const log = debug('application:bootstrap')
-
-// Enable debug messages outside of production
-if (process.env.NODE_ENV !== 'production') {
-  debug.enable('application:*')
-}
-
-log('creating application node')
-const applicationNode = document.createElement('div')
-applicationNode.className = 'application'
-applicationNode.id = 'application'
-
-log('adding application node to body')
-document.body.appendChild(applicationNode)
-
-log('mounting application')
-React.render(<Hello/>, applicationNode, () => {
-  log('finished mounting application')
-})
+React.render(<FancyInput />, DIV);
