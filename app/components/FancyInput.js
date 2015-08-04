fe5a577d336feb3a890b5d8491084d858dce8d42
Fernando <krajka.bartosz@gmail.com>
2015-08-04 15:55:16 +0200

    CSS defined. Also the web changed a little to see the difference.

--- app/components/FancyInput.js
+++ app/components/FancyInput.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class FancyInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    let newText = e.target.value;
+    this.setState({text: newText});
+  }
+
   render() {
-    return null;
+    let spanClass = (this.props.stamp) ? "stamp" : "placeholder";
+    return (
+      <div className="fancy-input">
+          <input type="text" value={this.state.text} onChange={this.handleInputChange}/>
+          <span className={spanClass} >{spanClass}</span>
+      </div>
+    );
   }
 }
 
--- app/index.js
+++ app/index.js
@@ -2,7 +2,10 @@ import './styles.css';
 import React from 'react';
 import FancyInput from './components/FancyInput';
 
-const DIV = document.createElement('div');
-document.body.appendChild(DIV);
+const DIV1 = document.createElement('div');
+document.body.appendChild(DIV1);
+const DIV2 = document.createElement('div');
+document.body.appendChild(DIV2);
 
-React.render(<FancyInput />, DIV);
+React.render(<FancyInput />, DIV1);
+React.render(<FancyInput stamp="true" />, DIV2);
