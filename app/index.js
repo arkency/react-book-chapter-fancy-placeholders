5439f46d997525e8e09209eda5f639fe50e53439
Paweł Pacana <pawel.pacana@gmail.com>
2017-07-21 22:42:22 +0200

    Upgrade React dependency.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 import './styles.css';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import FancyInput from './components/FancyInput';
 
 const DIV = document.createElement('div');
 document.body.appendChild(DIV);
 
-React.render(<FancyInput placeholder="First name"/>, DIV);
+ReactDOM.render(<FancyInput placeholder="First name"/>, DIV);
