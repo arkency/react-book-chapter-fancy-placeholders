e0076606c64f060cd2e236988393174903e23175
Paweł Pacana <pawel.pacana@gmail.com>
2017-08-16 18:25:06 +0200

    Runtime on create-react-app.
    https://github.com/facebookincubator/create-react-app

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FancyInput from './components/FancyInput';
-
-const DIV = document.createElement('div');
-document.body.appendChild(DIV);
-
-ReactDOM.render(<FancyInput placeholder="First name"/>, DIV);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import FancyInput from "./components/FancyInput";
+
+ReactDOM.render(
+  <FancyInput placeholder="First name" />,
+  document.getElementById("root")
+);
